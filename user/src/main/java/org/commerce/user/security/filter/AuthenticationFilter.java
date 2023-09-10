f584ffe30e9c104753eecfbdc6b1c618876f8b58
jo <chojh3123@gmail.com>
2023-09-10 23:28:38 +0900

    Feat: 로그인 성공시 토큰 발행

--- user/src/main/java/org/commerce/user/security/filter/AuthenticationFilter.java
+++ user/src/main/java/org/commerce/user/security/filter/AuthenticationFilter.java
@@ -7,14 +7,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.commerce.user.dto.LoginRequest;
+import org.commerce.user.security.jwt.TokenUtils;
+import org.commerce.user.security.model.SecuredUser;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Service;
-
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -41,7 +43,12 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        SecuredUser securedUser = (SecuredUser) authResult.getPrincipal();
+        String token = TokenUtils.generateToken(securedUser.getEmail(), LocalDateTime.now().plusDays(1));
+        response.setHeader("access_token", token);
     }
 }
