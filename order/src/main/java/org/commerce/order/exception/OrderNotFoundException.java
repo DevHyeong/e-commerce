b49ce10088e20238f8f2ee9d8a77c67e4341391d
jo <chojh3123@gmail.com>
2024-01-06 18:26:26 +0900

    Feat: Order 생성자 추가

--- order/src/main/java/org/commerce/order/exception/OrderNotFoundException.java
+++ order/src/main/java/org/commerce/order/exception/OrderNotFoundException.java
@@ -5,4 +5,8 @@ public class OrderNotFoundException extends RuntimeException{
     public OrderNotFoundException(Long orderId){
         super(String.format("주문번호 %s가 존재하지 않습니다.", orderId));
     }
+
+    public OrderNotFoundException(String message){
+        super(message);
+    }
 }
